9967e15fc8da088a6b5eccdb1cc3eaef07c37eda
t-hashibe <69305921+taiki-hashibe@users.noreply.github.com>
2025-06-17 20:37:09 +0900

    fix gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  build: {
+    outDir: "docs"
+  },
   plugins: [
     tailwindcss(),
   ],
